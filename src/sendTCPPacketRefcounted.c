cee3e56be1d2769a7ec116cb0fba81de906ab99d
wakoo1337 <wakoo1337@yandex.ru>
2024-08-09 00:45:44 +0300

    В sendTCPPacketRefcounted() теперь передаётся не элемент очереди пакетов, а элемент очереди отправляемых сегментов TCP

--- src/sendTCPPacketRefcounted.c
+++ src/sendTCPPacketRefcounted.c
@@ -15,19 +15,17 @@
 
 #include "sendTCPPacketRefcounted.h"
 unsigned int sendTCPPacketRefcounted(struct CaptureContext *context, uint8_t *packet, unsigned int size, void *arg) {
-	struct PacketQueueItem *item;
-	item = (struct PacketQueueItem *) arg;
-	struct TCPAppQueueItem *segment;
-	segment = (struct TCPAppQueueItem *) item->arg;
+	struct TCPAppQueueItem *item;
+	item = (struct TCPAppQueueItem *) arg;
 	ssize_t result;
 	result = context->settings->write_function(packet, size, context->settings->user);
 	if (-1 == result) return (errno != EAGAIN) && (errno != EWOULDBLOCK);
-	pthread_mutex_lock(&segment->connection->mutex);
-	segment->ref_count--;
-	if (0 == segment->ref_count) {
-		free(segment->free_me);
-		free(segment);
+	pthread_mutex_lock(&item->connection->mutex);
+	item->ref_count--;
+	if (0 == item->ref_count) {
+		free(item->free_me);
+		free(item);
 	};
-	pthread_mutex_unlock(&segment->connection->mutex);
+	pthread_mutex_unlock(&item->connection->mutex);
 	return 0;
 };
--- src/tunWriteCallback.c
+++ src/tunWriteCallback.c
@@ -17,10 +17,12 @@ void tunWriteCallback(evutil_socket_t fd, short what, void *arg) {
 	if (what & EV_WRITE) {
 		struct PacketQueueItem *item;
 		while (dequeueTxPacket(context, &item)) {
-			if (item->processor(context, item->data, item->count, item)) {
+			if (item->processor(context, item->data, item->count, item->arg)) {
+				free(item);
 				emergencyStop(context);
 				return;
 			};
+			free(item);
 		};
 		pthread_mutex_lock(&context->tx_mutex);
 		if ((NULL == context->tx_begin) && (-1 == event_del(context->tx_event))) event_base_loopbreak(context->event_base);
