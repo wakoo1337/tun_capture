ef4294daca6978c7eba60fd703e28da9cbabdc41
wakoo1337 <wakoo1337@yandex.ru>
2025-01-21 14:25:59 +0300

    Ещё неотправленные пакеты копируются в связный список перед отправкой, чтобы tcpCleanupConfirmed() их не удалила раньше времени

--- src/enqueueUnsentTCPPacketsTransmission.c
+++ src/enqueueUnsentTCPPacketsTransmission.c
@@ -1,6 +1,7 @@
 #include <pthread.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include <sys/socket.h>
 #include "SrcDstSockaddrs.h"
 #include "TCPAppQueueItem.h"
@@ -14,18 +15,34 @@
 
 #include "enqueueUnsentTCPPacketsTransmission.h"
 unsigned int enqueueUnsentTCPPacketsTransmission(struct TCPConnection *connection) {
+	struct ItemsLinkedList {
+		struct TCPAppQueueItem *item;
+		struct ItemsLinkedList *next;
+	} *first, **last;
+	first = NULL;
+	last = &first;
 	struct TCPAppQueueItem *current;
 	current = connection->app_queue;
 	while (current && current->is_filled) {
-		struct TCPAppQueueItem *next;
-		next = current->next;
 		if ((current->timeout == NULL) && isAppQueueItemInWindow(connection->latest_ack, connection->app_window, current)) {
-			incrementAppQueueItemRefCount(current); // Добавляем ещё одну ссылку, так как она у нас в локальной переменной
-			if (enqueueTCPPacketTransmission(current) || enqueueTCPRetransmission(current)) return 1; // TODO сделать нормальный способ удаления
-			decrementAppQueueItemRefCount(current);
-			freeNoRefsAppQueueItem(current);
+			struct ItemsLinkedList *new_item;
+			new_item = malloc(sizeof *new_item);
+			new_item->item = current;
+			incrementAppQueueItemRefCount(new_item->item);
+			new_item->next = NULL;
+			*last = new_item;
+			last = &new_item->next;
 		};
-		current = next;
+		current = current->next;
+	};
+	while (first) {
+		struct ItemsLinkedList *next;
+		next = first->next;
+		if (enqueueTCPPacketTransmission(first->item) || enqueueTCPRetransmission(first->item)) return 1; // TODO сделать нормальный способ удаления
+		decrementAppQueueItemRefCount(first->item);
+		freeNoRefsAppQueueItem(first->item);
+		free(first);
+		first = next;
 	};
 	return 0;
 };
